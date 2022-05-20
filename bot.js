ef0cae34e82bfeb2d21d4857e5abd4df7a0b3473
★. KuJu -TH- <plan291037@outlook.com>
2022-05-20 22:17:17 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,8 @@ const { paddedFullWidth, errorWrap } = require('./utils.js');
 // const fetch = require('@vercel/fetch')(require('node-fetch'));
 // const fetch = require('@vercel/fetch-retry')(require('node-fetch'));
 const fetch = require('@vercel/fetch')(require('node-fetch'));
+const retry = require('async-retry');
+const debug = require('debug')('fetch-retry');
 // -----------------------------------------------------------
 
 const LOG_LEVELS = {
@@ -77,7 +79,7 @@ exports.start = function(SETUP) {
   const UPDATE_TIME = SETUP.UPDATE_TIME; // in ms
   const MIN_TIMEOUT = 10;
   const MAX_RETRIES = SETUP.FETCH_TIMEOUT;
-  const MAX_RETRY_AFTER = 10;
+  const MAX_RETRY_AFTER = 20;
   const FACTOR = 6;
 
   var TICK_N = 0;
@@ -103,17 +105,105 @@ exports.start = function(SETUP) {
 //     }
 // };
 
- const fetch_retry = async (url, options, n=FETCHTEST_LOOP) => {
-     let error;
-     for (let i = 0; i < n; i++) {
+ // const fetch_retry = async (url, options, n=FETCHTEST_LOOP) => {
+ //     let error;
+ //     for (let i = 0; i < n; i++) {
+ //        try {
+ //             return await fetch(url, options);
+ //         } catch (err) {
+ //             error = err;
+ //         }
+ //     }
+ //     throw error;
+ // };
+
+  async function fetch_retry(url, opts = {}) {
+    const retryOpts = Object.assign(
+      {
+        // timeouts will be [10, 60, 360, 2160, 12960]
+        // (before randomization is added)
+        minTimeout: MIN_TIMEOUT,
+        retries: MAX_RETRIES,
+        factor: FACTOR,
+        maxRetryAfter: MAX_RETRY_AFTER,
+      },
+      opts.retry
+    );
+
+    if (opts.onRetry) {
+      retryOpts.onRetry = (error) => {
+        opts.onRetry(error, opts);
+        if (opts.retry && opts.retry.onRetry) {
+          opts.retry.onRetry(error);
+        }
+      };
+    }
+
+    try {
+      return await retry(async (bail, attempt) => {
+        const { method = 'GET' } = opts;
         try {
-             return await fetch(url, options);
-         } catch (err) {
-             error = err;
-         }
-     }
-     throw error;
- };
+          // this will be retried
+          const res = await fetch(url, opts);
+          debug('status %d', res.status);
+          if ((res.status >= 500 && res.status < 600) || res.status === 429) {
+            // NOTE: doesn't support http-date format
+            const retryAfter = parseInt(res.headers.get('retry-after'), 10);
+            if (retryAfter) {
+              if (retryAfter > retryOpts.maxRetryAfter) {
+                return res;
+              } else {
+                await new Promise((r) => setTimeout(r, retryAfter * 1e3));
+              }
+            }
+            throw new ResponseError(res);
+          } else {
+            return res;
+          }
+        } catch (err) {
+          if (err.type === 'aborted') {
+            return bail(err);
+          }
+          const clientError = isClientError(err);
+          const isRetry = !clientError && attempt <= retryOpts.retries;
+          debug(
+            `${method} ${url} error (status = ${err.status}). ${
+              isRetry ? 'retrying' : ''
+            }`,
+            err
+          );
+          if (clientError) {
+            return bail(err);
+          }
+          throw err;
+        }
+      }, retryOpts);
+    } catch (err) {
+      if (err instanceof ResponseError) {
+        return err.res;
+      }
+      throw err;
+    }
+  }
+
+  class ResponseError extends Error {
+  constructor(res) {
+    super(res.statusText);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ResponseError);
+    }
+
+    this.name = this.constructor.name;
+    this.res = res;
+
+    // backward compat
+    this.code = this.status = this.statusCode = res.status;
+    this.url = res.url;
+  }
+}
+
+exports.ResponseError = ResponseError;
 
   async function getPlayers() {
     
